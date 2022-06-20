7fd1952688a0b33d6b0f2ba0a61d0fc93d3ef739
unknown <kinekarpallavi21@gmail.com>
2022-06-21 00:15:23 +0530

    [Pallavi]Add:Function expression(anonymous)example

---
+++ FunctionExpreassion.js
@@ -0,0 +1,8 @@
+/*program for Function Expression (anonymous) return to perform substraction */
+// declaring a function
+var calSub = function(x, y){
+    let z = x - y;
+    return z;
+}
+
+console.log("Subtraction : " + calSub(7, 4));
